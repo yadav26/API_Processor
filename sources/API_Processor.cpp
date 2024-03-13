43cf59691f78171c67ffd4259ce53a0597ae0550
Anshul Yadav <yadav26@gmail.com>
2024-03-13 19:01:50 +1100

    cosmetic

--- sources/API_Processor.cpp
+++ sources/API_Processor.cpp
@@ -14,18 +14,15 @@ using namespace std;
 
 int main()
 {
-    //std::thread cmdEditor([&]() {
-    //    auto mgr = CmdManager::GetCmdManager();
-    //    mgr.start();
-    //});
-    //cmdEditor.join();
-  
-    std::thread byteFeeder([&]() {
-        cout << "\n------Byte stream reader started successfully.\n";
-        CmdLineStreamReader sr;
-        sr.start();
+    std::thread cmdEditor([&]() {
+       auto mgr = CmdManager::GetCmdManager();
+       mgr.start();
     });
-    byteFeeder.join();
+    cmdEditor.join();
+  
+    CmdLineStreamReader sr;
+    sr.start();
+    
     return 0;
 }
 
--- sources/CommandHandlers/CmdAttributesValidator.hpp
+++ sources/CommandHandlers/CmdAttributesValidator.hpp
@@ -31,19 +31,19 @@ public:
 			string name = v[i];
 			if (!CmdAttributeStore::GetInstance().IsValidAttrib(name))
 			{
-				cout << "\n Invalid attribute name " << name;
+				//cout << "\n Invalid attribute name " << name;
 				err = "Invalid attribute name" ;
 				return false;
 			}
 			if (!CmdAttributesMapper::GetInstance().ValidAttributeForCmd(cmd, name))
 			{
-				cout << "\n Invalid cmd attribute pair: " << cmd << ", " << name;
+				//cout << "\n Invalid cmd attribute pair: " << cmd << ", " << name;
 				err = "Attribute not associated.";
 				return false;
 			}
 			auto len = atoi(v[i + 1].c_str());
 			if ((int)v[i + 2].size() != len) {
-				printf("\n attrib len match failed. expected(%d), actual(%d)", (int)v[i + 2].size(), len);
+				//printf("\n attrib len match failed. expected(%d), actual(%d)", (int)v[i + 2].size(), len);
 				stringstream ss;
 				ss << ("Attribute len match failed.expected(");
 				ss << (int)v[i + 2].size();
@@ -56,7 +56,7 @@ public:
 				continue;
 			for (auto& c : v[i + 2]) {
 				if (c != '.' && !isdigit(c)) {
-					cout << "\n attrib amount invlaid digit : " << v[i + 2];
+					//cout << "\n attrib amount invlaid digit : " << v[i + 2];
 					err = "Attrib amount is not a valid digit.(" + v[i + 2] + ")";
 					return false;
 				}
